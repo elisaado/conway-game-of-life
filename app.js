b8b4fb6cc46ad0977abb1c5aaf5b2485e42a7baf
Eli Saado <eli5saado@gmail.com>
2021-06-15 17:58:45 +0200

    Add click handler boilerplate

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const windowHeight = window.innerHeight;
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 
+canvas.addEventListener("click", click, false);
+
+function click(e) {
+  console.log(`${e.pageX} ${e.pageY}`);
+}
+
 function draw() {
   for (let y = 0; y < cellSize * windowHeight; y += cellSize) {
     for (let x = 0; x < cellSize * windowWidth; x += cellSize) {
