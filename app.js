0a41a093d99c49949a64cb77dcb4dda8a6220da1
Eli Saado <eli5saado@gmail.com>
2021-06-15 15:19:51 +0200

    Add draw function (it's slow)

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+const cellSize = 12; // 12x12 px
+const fps = 1;
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+function draw() {
+  for (let x = 0; x < cellSize * windowWidth; x += cellSize) {
+    for (let y = 0; y < cellSize * windowHeight; y += cellSize) {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(cellSize + x, y);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x, cellSize + y);
+      ctx.stroke();
+    }
+  }
+}
+
+setInterval(draw, 1000 / fps);
